41e3d09041d2f04fd382c701a05af101d646c2d5
Gahyung Kim <ghkim10202@gmail.com>
2022-03-22 19:00:29 +0900

    Add problem 4375

--- baekjoon/Fundamental/math.py
+++ baekjoon/Fundamental/math.py
@@ -1,11 +1,19 @@
 # 백준 10430 - 나머지
-a, b, c = map(int, input().split())
-first = (a + b) % c
-second = ((a % c) + (b % c)) % c
-third = (a * b) % c
-fourth = ((a % c) * (b % c)) % c
+# a, b, c = map(int, input().split())
+# first = (a + b) % c
+# second = ((a % c) + (b % c)) % c
+# third = (a * b) % c
+# fourth = ((a % c) * (b % c)) % c
 
-print(first)
-print(second)
-print(third)
-print(fourth)
+# print(first)
+# print(second)
+# print(third)
+# print(fourth)
+
+# 백준 4375번 - 1
+n = int(input())
+for i in range(2, 9999):
+    num = int("1" * i)
+    if num % n == 0:
+        print(len(str(num)))
+        break
