3c65d94c3bf8633416ba67eeb7c390e6cb1bd3f4
Gahyung Kim <ghkim10202@gmail.com>
2022-03-22 20:14:45 +0900

    Add problem 2609

--- baekjoon/Fundamental/math.py
+++ baekjoon/Fundamental/math.py
@@ -43,3 +43,15 @@ for i in range(N):
     for i in range(1, num + 1):
         sum_ += (n // i) * i
     print(sum_)
+
+# 백준 2609번 - 최대공약수와 최소공배수
+a, b = map(int, input().split())
+for i in range(min(a, b), 0, -1):
+    if a % i == 0 and b % i == 0:
+        print(i)
+        break
+
+for i in range(max(a, b), a * b):
+    if i % a == 0 and i % b == 0:
+        print(i)
+        break
