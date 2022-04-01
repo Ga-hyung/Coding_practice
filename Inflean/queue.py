ac2757abc03e48eb6c405d69cda0ef61f474cb7a
Gahyung Kim <ghkim10202@gmail.com>
2022-04-01 21:19:00 +0900

    Finish Queue

--- Inflean/queue.py
+++ Inflean/queue.py
@@ -1,19 +1,19 @@
 from collections import deque
 
-# # 공주 구하기 (큐 자료구조로 풀기)
+# 공주 구하기 (큐 자료구조로 풀기)
 
-# n, k = map(int, input().split())
-# p = deque(list(range(1, n + 1)))
+n, k = map(int, input().split())
+p = deque(list(range(1, n + 1)))
 
-# while p:
-#     for _ in range(k - 1):  # 1 2
-#         p.append(p.popleft())
-#     p.popleft()
-#     if len(p) == 1:
-#         print(p[0])
-#         p.popleft()
+while p:
+    for _ in range(k - 1):  # 1 2
+        p.append(p.popleft())
+    p.popleft()
+    if len(p) == 1:
+        print(p[0])
+        p.popleft()
 
-############################################################
+#############################################################################
 
 # 응급실 (큐)
 n, m = map(int, input().split())
@@ -29,3 +29,23 @@ while True:
         if cur[0] == m:
             break
 print(cnt)
+
+#############################################################################
+
+# 교육과정 설계
+man = list(input())
+n = int(input())
+
+for i in range(n):
+    plan = list(input())
+    dq = deque(man)  # 과목이 들어올 떄 마다 초기화하기
+    for x in plan:
+        if x in dq:
+            if x != dq.popleft():
+                print(f"#{i+1} NO")
+                break
+    else:  # 순서가 통가가 됨
+        if len(dq) == 0:
+            print(f"{i+1} YES")
+        else:
+            print(f"{i+1} NO")
