1b8798c32fb63ef0ecb9294253588617d3cd08f3
Gahyung Kim <ghkim10202@gmail.com>
2022-04-02 20:13:33 +0900

    Add DFS problem

--- Inflean/DFS.py
+++ Inflean/DFS.py
@@ -104,3 +104,54 @@ result = 0
 tsum = sum(weight)
 DFS(0, 0, 0)
 print(result)
+
+################################################################
+
+# 중복수열 구하기
+
+
+def DFS(idx):
+    global cnt
+    if idx == m:  # 중복순열이 완성되었을 떄
+        for x in res:
+            print(x, end=" ")
+        print()
+        cnt += 1
+    else:
+        for i in range(1, n + 1):
+            res[idx] = i
+            DFS(idx + 1)
+
+
+n, m = map(int, input().split())
+res = [0] * m
+cnt = 0
+DFS(0)
+print(cnt)
+
+#################################################################
+
+# 동전 교환
+def DFS(idx, sum_):  # idx는 동전의 갯수
+    global res
+    if idx > res:  # 제일 작은 count(idx) 값보다 더 크게 나아가지 않도록 막는 것
+        return
+    if sum_ > m:
+        return
+    if sum_ == m:
+        if idx < res:
+            res = idx
+    else:
+        for i in range(n):
+            DFS(idx + 1, sum_ + coin[i])
+
+
+n = int(input())
+coin = list(map(int, input().split()))
+m = int(input())
+res = 2147000
+coin.sort(reverse=True)  # 가장 큰 동전부터 DFS가 시작되므로 효율적이다.
+DFS(0, 0)
+print(res)
+
+#################################################################
